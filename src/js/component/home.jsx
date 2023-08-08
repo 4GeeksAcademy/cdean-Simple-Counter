d460f1b948006e1cf3371d1613521f1a53b2b536
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-08-08 20:46:18 +0000

    Upload

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,14 @@
 import React from "react";
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import SecondCounter from "./secondsCounter.js";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = ({unidad, decena, centena, unidadMil, decenaMil, centenaMil}) => {
+    return (
+        <div className="container-full">
+        <SecondCounter unidad={unidad} decena={decena} centena={centena} unidadMil={unidadMil} decenaMil={decenaMil} centenaMil={centenaMil}/>
+        </div>
+    );
 };
-
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,49 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let unidad = 0;
+let decena = 0;
+let centena = 0;
+let unidadMil = 0;
+let decenaMil = 0;
+let centenaMil = 0;
+
+function displayHello() {
+    //render your react application
+    //            qué vamos a dibujar,    donde lo vamos a dibujar
+    ReactDOM.render(<Home unidad={unidad} decena={decena} centena={centena} unidadMil={unidadMil} decenaMil={decenaMil} centenaMil={centenaMil}/>, document.querySelector("#app"));
+    unidad++;
+    
+    if (unidad > 9) {
+        decena++;
+        unidad = 0;
+    }
+
+    if (decena > 9) {
+        centena++;
+        decena = 0;
+    }
+
+    if (centena > 9) {
+        unidadMil++;
+        centena=0;
+    }
+
+    if (unidadMil > 9) {
+        decenaMil++
+        unidadMil = 0;
+    }
+
+    if (decenaMil > 9) {
+        centenaMil++;
+        decenaMil = 0;
+    }
+
+
+}
+
+setInterval(displayHello, 1000);
